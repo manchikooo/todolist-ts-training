d9caed026f6fa0936a92560fb5df2528658d5b04
manchikooo <manchiko.man31@mail.ru>
2022-01-26 11:55:11 +0300

    Updates

---
+++ precache-manifest.b5db7c6d341315d4de3dd9594a27228b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37144719bb34a948551e176bed8eeb3b",
+    "url": "/todolist-ts-training/index.html"
+  },
+  {
+    "revision": "e682430ba744fc5fe17d",
+    "url": "/todolist-ts-training/static/css/main.afdba4d7.chunk.css"
+  },
+  {
+    "revision": "f35d516660793da1907e",
+    "url": "/todolist-ts-training/static/js/2.080391ff.chunk.js"
+  },
+  {
+    "revision": "33628a052932c5622bd1ac5b94a511a7",
+    "url": "/todolist-ts-training/static/js/2.080391ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e682430ba744fc5fe17d",
+    "url": "/todolist-ts-training/static/js/main.ea351464.chunk.js"
+  },
+  {
+    "revision": "e87f3f00451e3c6d86c3",
+    "url": "/todolist-ts-training/static/js/runtime-main.1cf05bde.js"
+  }
+]);
